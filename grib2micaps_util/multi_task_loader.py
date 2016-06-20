08d90d70270658995e91bc559fbad2ff145f3326
perillaroc <perillaroc@gmail.com>
2016-06-20 16:15:37 +0800

    Add xml config file.

--- grib2micaps_util/multi_task_loader.py
+++ grib2micaps_util/multi_task_loader.py
@@ -3,18 +3,56 @@ from multiprocessing import Pool
 import os
 import sys
 import datetime
+from xml.dom import minidom
+from xml.dom import Node
 
-program_dir_path = 'D:/windroc/project/2016/grib2micaps/grapesGfs_grib2micaps_v3.9.4'
-program_file_name = 'grib2micaps.exe'
+
+class Config(object):
+    def __init__(self, config_path):
+        self.config_path = config_path
+        self.config_dict = self.load_config()
+
+    def load_config(self):
+        config_dict = dict()
+        doc = minidom.parse(self.config_path)
+        root = doc.documentElement
+
+        pool_size_node = root.getElementsByTagName('pool_size')[0]
+        config_dict['pool_size'] = int(pool_size_node.firstChild.nodeValue)
+
+        task_list_node = root.getElementsByTagName('task_list')[0]
+
+        task_list = []
+        for task_node in task_list_node.childNodes:
+            if task_node.nodeType == Node.ELEMENT_NODE:
+                one_task = {}
+                for variable_node in task_node.childNodes:
+                    if variable_node.nodeType == Node.ELEMENT_NODE:
+                        one_task[variable_node.nodeName] = variable_node.firstChild.nodeValue
+
+                task_list.append(one_task)
+
+        config_dict['task_list'] = task_list
+
+        return config_dict
 
 
-def run_grib2micaps(no, date, start_hour, end_hour):
+def run_grib2micaps(no, task_param):
+
     print('start task {no} on {pid}...'.format(no=no, pid=os.getpid()))
-    os.chdir(program_dir_path+'_{no}'.format(no=no))
+    os.chdir(task_param['run_dir'])
     task_start_time = datetime.datetime.now()
-    subprocess.run([program_file_name, date, start_hour, end_hour],
-                   stdout=None, stderr=None,
-                   shell=True)
+    subprocess.run(
+        [
+            task_param['program'],
+            task_param['start_time'],
+            task_param['start_forecast_hour'],
+            task_param['end_forecast_hour']
+        ],
+        stdout=None,
+        stderr=None,
+        shell=False
+    )
     task_end_time = datetime.datetime.now()
     print('end task {no} on {pid} in {run_time}'.format(
         no=no,
@@ -26,19 +64,17 @@ def run_grib2micaps(no, date, start_hour, end_hour):
 def main():
     print('Loader start...')
     loader_start_time = datetime.datetime.now()
-    os.chdir(program_dir_path)
 
-    p = Pool(2)
+    config = Config(os.path.dirname(__file__) + '/conf/multi_task_loader.config.xml')
 
-    args_list = [
-        (0, '2016061708', '0', '3'),
-        (1, '2016061708', '6', '9'),
-    ]
+    p = Pool(config.config_dict['pool_size'])
 
-    for i in range(2):
-        p.apply_async(run_grib2micaps, args=args_list[i])
+    for i in range(len(config.config_dict['task_list'])):
+        p.apply_async(run_grib2micaps, args=(i, config.config_dict['task_list'][i]))
+
     p.close()
     p.join()
+
     loader_end_time = datetime.datetime.now()
     print("All task finished in {run_time}.".format(
         run_time=loader_end_time-loader_start_time
